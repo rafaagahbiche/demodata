be6e96a419e7a155d2063a6b19d9462879555c12
rafaagahbiche <grafaa@hotmail.fr>
2017-07-04 13:15:04 +0200

    Add files via upload

---
+++ demo/demo.data/demo.data/Concrete/DataContext.cs
@@ -0,0 +1,27 @@
+﻿namespace demo.data
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Xml.Linq;
+
+	public class DataContext: IDataContext
+	{
+		//private static readonly string _path = "@/Data/data.xml";
+		private XDocument _dataXml;
+		public DataContext(string _path)
+		{
+			_dataXml = XDocument.Load(_path);
+		}
+
+		public XDocument DataXml
+		{
+			get
+			{
+				return _dataXml;
+			}
+		}
+	}
+}
---
+++ demo/demo.data/demo.data/Concrete/PageData.cs
@@ -0,0 +1,8 @@
+﻿namespace demo.data
+{
+	public class PageData: Item
+	{
+		public string Content { get; set; }
+		public int ArticleId { get; set; }
+	}
+}
